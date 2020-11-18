6d1feb099cba63e97dfa836fefc05f3c9f7be35b
KvngJay24 <43212074+KvngJay24@users.noreply.github.com>
2020-11-18 16:31:16 -0500

    changed ip_addr to ip

--- nmapscript.py
+++ nmapscript.py
@@ -8,9 +8,9 @@ print('Welcome to my basic nmap port scanner')
 print(' ')
 print('<--------------------------------------------------------->')
 
-ip_addr = input("Please input the IP address you wish to scan: ")
-print('The IP you entered is: ', ip_addr)
-type(ip_addr)
+ip = input("Please input the IP address you wish to scan: ")
+print('The IP you entered is: ', ip)
+type(ip)
 
 resp=input(""" \nWhat type of scan would you like to execute?
 	1) SYN ACK Scan
@@ -22,24 +22,24 @@ print('You have selected option: ', resp)
 #First Scan
 if resp == '1':
 	print('Nmap version: '), scanner.nmap_version()
-	scanner.scan(ip_addr,'1-1024','-v -sS')
+	scanner.scan(ip,'1-1024','-v -sS')
 	print(scanner.scaninfo())
-	print('IP Status: ', scanner[ip_addr].state)
-	print(scanner[ip_addr].allprotocols())
-	print('Open Ports: ', scanner[ip_addr]['tcp'].keys())
+	print('IP Status: ', scanner[ip].state)
+	print(scanner[ip].allprotocols())
+	print('Open Ports: ', scanner[ip]['tcp'].keys())
 elif resp=='2':
-	print("Namp Version: ", scanner.nmap_version())
-	scanner.scan(ip_addr, '1-1024', '-v -sU')
+	print("Nmap Version: ", scanner.nmap_version())
+	scanner.scan(ip, '1-1024', '-v -sU')
 	print(scanner.scaninfo())
-	print("IP Status: ", scanner[ip_addr].state())
-	print(scanner[ip_addr].all_protocols())
-	print("Open Ports: ", scanner[ip_addr]['udp'].keys())
+	print("IP Status: ", scanner[ip].state())
+	print(scanner[ip].all_protocols())
+	print("Open Ports: ", scanner[ip]['udp'].keys())
 elif resp == '3':
-	print("Namp Version: ", scanner.nmap_version())
-	scanner.scan(ip_addr, '1-1024', '-v -sS -sV -sC -A -O')
+	print("Nmap Version: ", scanner.nmap_version())
+	scanner.scan(ip, '1-1024', '-v -sS -sV -sC -A -O')
 	print(scanner.scaninfo())
-	print("IP Status: ", scanner[ip_addr].state())
-	print(scanner[ip_addr].all_protocols())
-	print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
-else resp >= '4':
+	print("IP Status: ", scanner[ip].state())
+	print(scanner[ip].all_protocols())
+	print("Open Ports: ", scanner[ip]['tcp'].keys())
+elif resp >= '4':
 	print("Input invalid! Please input valid option.")
